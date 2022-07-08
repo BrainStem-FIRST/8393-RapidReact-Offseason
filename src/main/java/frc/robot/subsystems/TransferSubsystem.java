add702935f4d62f6a41ac65a7e4980cfa1c9a7f2
Dave Whitmer <david.whitmer@me.com>
2022-07-08 12:23:53 -0400

    TransferCommand and TransferSubSystem Classes

--- src/main/java/frc/robot/subsystems/TransferSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransferSubsystem.java
@@ -7,13 +7,14 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.TransferConstants;
 
 public class TransferSubsystem extends SubsystemBase {
 
-    TransferConstants constants = new TransferConstants();
-    public CANSparkMax transfer_motor = new CANSparkMax(TransferConstants.transferMotorPort, MotorType.kBrushless);
-    
+    CANSparkMax transfer_motor = new CANSparkMax(transferMotorPort, MotorType.kBrushless);
+
+    public void motor_on() {}
+        
+   
     public TransferSubsystem(){
     }
 
