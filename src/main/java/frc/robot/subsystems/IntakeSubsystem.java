4379da9e76cead784c6dd09973b05f4d1009e07d
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-07-17 14:24:44 -0400

    cleaned up code

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,22 +2,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
-import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-//import frc.robot.commands.DefaultDriveCommand;
-//import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.Constants.PnuematicsConstants;
-import frc.robot.Constants.IntakeConstants;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
@@ -44,11 +32,11 @@ public class IntakeSubsystem extends SubsystemBase implements AutoCloseable{
     return intakePneumatics.get() == DoubleSolenoid.Value.kForward;
   }
 
-  public boolean isIntakeReadyToRetract() { // FIXME
+  public boolean isIntakeReadyToRetract() { // FIXME - More may need to be added
     return isPneumaticsExtended() && isIntakeMotorRunningAtRightSpeed();
   }
 
-  public boolean isIntakeReadyToDeploy() { // FIXME
+  public boolean isIntakeReadyToDeploy() { // FIXME - More may need to be added
     return isIntakeMotorStopped() && isPneumaticsRetracted();
   }
 
@@ -58,7 +46,6 @@ public class IntakeSubsystem extends SubsystemBase implements AutoCloseable{
   }
 
   public void end() {
-    // HOW DO YOU RETURN WITH IF IN FRONT?
     if (isIntakeReadyToRetract()) {
       retractIntake();
     }
@@ -93,9 +80,11 @@ public class IntakeSubsystem extends SubsystemBase implements AutoCloseable{
       retractIntake();
     }
   }
+
   @Override
   public void close() throws Exception {
     intakeMotor.close();
     intakePneumatics.close();
   }
+  
 }
