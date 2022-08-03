ad6db2ce390cc18752b1ad9b6eb16ef189c0e414
Mihir Sharma <mihirsharma700@gmail.com>
2022-08-03 17:11:38 -0400

    fixed-transfer-error

--- src/test/java/TransferSubsystemTest.java
+++ src/test/java/TransferSubsystemTest.java
@@ -1,13 +1,12 @@
 import java.beans.Transient;
 
 import org.junit.*;
-import net.thefletcher.revrobotics.CANSparkMax;
+//import net.thefletcher.revrobotics.CANSparkMax;
 
 public class TransferSubsystemTest {
 
     @Test
     public void testTransferMotor() {
-        
         
     }
 
