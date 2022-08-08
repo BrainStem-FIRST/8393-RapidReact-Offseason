4caaf7dc24848309789929ab8c96b3dcae6db140
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-08-08 15:35:42 -0400

    not working

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.Driver1ControllerConstants;
 import frc.robot.Constants.Driver2ControllerConstants;
 import frc.robot.Constants.JoystickConstants;
@@ -57,110 +58,33 @@ public class RobotContainer {
         () -> driver1Controller.getRawAxis(JoystickConstants.RIGHT_STICK_X_AXIS),
         () -> !driver1Controller.getRawButton(JoystickConstants.LEFT_BUMPER)));
 
-    intakeSubsystem.setDefaultCommand(new DefaultIntakeCommand(intakeSubsystem,
-        true));
+ 
     shooterSubsystem.setDefaultCommand(
         new DefaultShooterCommand(shooterSubsystem, driver2Controller.getRawAxis(JoystickConstants.LEFT_STICK_Y_AXIS),
             driver2Controller.getRawAxis(JoystickConstants.RIGHT_STICK_X_AXIS),
             driver2Controller.getRawAxis(JoystickConstants.RIGHT_STICK_Y_AXIS)));
     transferSubsystem.setDefaultCommand(new DefaultTransferCommand(transferSubsystem, true));
+  
 
-    // Configure the button bindings
     configureButtonBindings();
-    // COMMANDS
-    /*
-     * new CompressorCommand(compressorSubsystem,
-     * Constants.PnuematicsConstants.COMPRESSOR_MIN_PRESSURE,
-     * Constants.PnuematicsConstants.COMPRESSOR_MAX_PRESSURE);
-     * new IntakeCommand(intakeSubsystem, 0, false);
-     */
+
 
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-   * it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+ 
   private void configureButtonBindings() {
-    // Back button zeros the gyroscope
-    /*
-     * new Button(driver1Controller::getBackButton)
-     * .whenPressed(drivetrainSubsystem::zeroHeading);
-     * // INTAKE CONTROLS
-     * new Button(driver1Controller::getYButton)
-     * .whenActive(() -> intakeSubsystem.toggleIntake(true)); //FIXME//FIGURE OUT
-     * HOW TO PASS IN DIFFERENT PARAMETERS WHEN ACTIVE IS FALSE
-     * // TRANSFER CONTROLS
-     * new Button(driver1Controller::getXButton)
-     * .whenActive(() -> transferSubsystem.toggleTransfer(true)); //FIXME
-     * 
-     * new Button(driver1Controller::getAButton)
-     * .whenActive(() -> liftCommandButton.buttonHit()); // may need to change to
-     * whenpressed //FIXME
-     */
+      new JoystickButton(driver1Controller, 3)
+        .toggleWhenActive(new DefaultShooterCommand(shooterSubsystem, 0.5, driver1Controller.getRawAxis(1), driver1Controller.getRawAxis(3)));
+
+     
   }
 
   /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
 
-    /*
-     * TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-     * AutoConstants.autoMaxSpeedMetersPerSecond,
-     * AutoConstants.autoMaxAccelerationMetersPerSecondSquared).setKinematics(
-     * AutoConstants.autoDriveKinematics);
-     * Trajectory trajectory = TrajectoryGenerator.generateTrajectory(new Pose2d(0,
-     * 0, new Rotation2d(0)),
-     * List.of(new Translation2d(1, 0), new Translation2d(1, -1)),
-     * new Pose2d(2, -1,
-     * Rotation2d.fromDegrees(180)),
-     * trajectoryConfig);
-     * 
-     * PIDController xController = new PIDController(AutoConstants.autoXController,
-     * 0, 0);
-     * PIDController yController = new PIDController(AutoConstants.autoYController,
-     * 0, 0);
-     * ProfiledPIDController thetaController = new ProfiledPIDController(
-     * AutoConstants.autoThetaController, 0, 0,
-     * Constants.AutoConstants.autoThetaControllerConstraints);
-     * thetaController.enableContinuousInput(-Math.PI, Math.PI);
-     * SwerveControllerCommand swerveControllerCommand = new
-     * SwerveControllerCommand(
-     * trajectory,
-     * drivetrainSubsystem::getPose,
-     * AutoConstants.autoDriveKinematics,
-     * xController,
-     * yController,
-     * thetaController,
-     * drivetrainSubsystem::setModuleStates,
-     * drivetrainSubsystem);
-     * 
-     * return new SequentialCommandGroup(
-     * new InstantCommand(() ->
-     * drivetrainSubsystem.resetOdometry(trajectory.getInitialPose())),
-     * swerveControllerCommand,
-     * new InstantCommand(() -> drivetrainSubsystem.stopModules()));
-     * 
-     * // An ExampleCommand will run in autonomous
-     * 
-     * m_drivetrainSubsystem.setDefaultCommand(new PathCommand(
-     * m_drivetrainSubsystem,
-     * () -> 0,
-     * () -> 0,
-     * () -> 20
-     * ));
-     * 
-     * return new InstantCommand();
-     */
-    // drivetrainSubsystem.setDefaultCommand(new
-    // DrivetrainTestCommand(drivetrainSubsystem, 1000));
+
 
     return new InstantCommand();
 
