045a1f47620e3f3a5d2e721681f2cff31ec61145
karthikpalled <77648774+karthikpalled@users.noreply.github.com>
2022-08-27 23:26:28 -0400

    updated

--- src/main/java/frc/robot/subsystems/TestCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/TestCameraSubsystem.java
@@ -1,10 +1,14 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.cscore.AxisCamera;
 import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.I2C.Port;
+
 import static frc.robot.Constants.*;
+import edu.wpi.first.cameraserver.CameraServer;
 
 
 public class TestCameraSubsystem {
-    cam UsbCamera = new UsbCamera(CameraConstans.TEST_CAMERA_NAME, CameraConstans.TEST_CAMERA_PORTID);
+    AxisCamera camera = AxisCamera.getHttpCameraKindFromInt(11);
 
 }
